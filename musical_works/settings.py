179ab8b8e9c08dc3fd80d5e7d81f6fe0fba75fc4
mkiterian <hmukite@gmail.com>
2019-10-29 14:13:27 +0300

    add ingestion command

--- musical_works/settings.py
+++ musical_works/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'musical_works.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'works',
+        'USER': 'works_admin',
+        'PASSWORD': 'asdfmovie123%',
+        'HOST': 'localhost',
+        'PORT': 5432
     }
 }
 
